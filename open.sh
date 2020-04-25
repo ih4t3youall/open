920e128dd6e56ff430139137f33096656b6f59be
Juan Martin Lequerica <juan.martin.lequerica@jmlequerica-ch--ul>
2020-04-25 11:20:06 -0500

    adding more support

--- open.sh
+++ open.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 filename=$(basename -- "$1")
 extension="${filename##*.}"
@@ -11,14 +11,14 @@ if [ $1 == "." ]; then
 fi
 
 
-if [ $extension = "sh" ]; then
+if [ $extension == "sh" ]; then
   chmod +x "$1"
   ./"$1"
   flag=1
 fi
 
 if [ $flag == 0 ]; then
-	if [ $extension = "jpg" ] || [ $extension = "jpeg" ] || [$extension = "png"]; then
+	if [ $extension == "jpg" ] || [ $extension == "jpeg" ] || [$extension == "png"] || [$extension == "bmp"]; then
 	  feh "$1"
 	  flag=1
 	fi
@@ -29,35 +29,37 @@ if [ $flag == 0 ]; then
 		input="/home/juan.martin.lequerica/.scripts/open/allVideoFilesLowercase.txt"
 		while IFS= read -r line
 			do
-				if [ $extension = "$line" ];then
-					vlc $1
+				if [ $extension == "$line" ];then
+					vlc "$1"
+	  				flag=1
 				fi
 		done < "$input"
-	  	flag=1
 fi
 
 
-if [ $extension = "html" ]; then
+if [ $extension == "html" ]; then
   opera "$1"
   flag=1
 fi
 
-if [ $extension = "txt" ]; then
+if [ $extension == "txt" ] || [ $extension == "java" ] || [ $extension == "feature" ]; then
   geany "$1"
   flag=1
 fi
 
-if [ $extension = "pdf" ]; then
+if [ $extension == "pdf" ]; then
   opera "$1"
   flag=1
 fi
 
-if [ $extension = "feature" ]; then
+if [ $flag == 0 ] && [ $extension == "feature" ]; then
   vim "$1"
   flag=1
 fi
 
-if [ $flag == 0 ]; then
+grep -q "\x00" $1 && value="Binary" || value="Text"
+
+if [ $flag == 0 ] && [ $value == "Text" ]; then
   vim "$1"
   flag=0
 fi
