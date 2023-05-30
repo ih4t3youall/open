63e8d7e15c313b246fa599cfb7dfc1ef385dd89d
martin lequerica <ih4t3youall@gmail.com>
2023-05-30 20:50:27 -0300

    enhanced by chat gpt

--- open.sh
+++ open.sh
@@ -1,101 +1,76 @@
 #!/bin/bash
-#open2
 
 filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-vim=0
-vlc=0
+# Function to open a file using the default application associated with its extension
+open_file() {
+    xdg-open "$1" >/dev/null 2>&1
+}
 
-office=("odt" "docx" "doc" "ods" "cvs")
-text=("txt" "java" "feature" )
-images=("jpg" "jpeg" "png" "bmp")
-video=("3gp" "asf" "avi" "flv" "mkv" "midi" "mp4" "ogg" "ogm" "wav" "mpeg" -2"ps" "ts" "pva" "mp3" "aiff" "mxf" "vob" "rm" "vcd" "svcd" "dvb" "heif" "avif" "aac" "ac3" "alac" "amr" "dts" "xm" "flac" "it" "mp3" "qcp" "qdm2" "tta" "wma" "dvb" "dvb" "dvb" "dvb" "atsc" "mpeg" "rtp" "rtsp" "3gpp" "pss" "mpeg" "mpl2" "ogm" "dv" "mpeg" "avc" "h" "mpeg" "hevc" "av1" "huffyuv" "mjpeg" "mpeg" "mpeg" "mpeg" "vc" "vp5" "vp6" "vp8" "vp9" "dnxhd" "wmv" "mov" "MOV")
+# Array of supported file types and their corresponding default applications
+declare -A file_types=(
+    ["odt"]="libreoffice"
+    ["docx"]="libreoffice"
+    ["doc"]="libreoffice"
+    ["ods"]="libreoffice"
+    ["csv"]="libreoffice"
+    ["txt"]="featherpad"
+    ["java"]="featherpad"
+    ["feature"]="featherpad"
+    ["jpg"]="feh"
+    ["jpeg"]="feh"
+    ["png"]="feh"
+    ["bmp"]="feh"
+    ["3gp"]="vlc"
+    ["asf"]="vlc"
+    ["avi"]="vlc"
+    ["flv"]="vlc"
+    ["mkv"]="vlc"
+    ["midi"]="vlc"
+    ["mp4"]="vlc"
+    # Add more file types and applications as needed
+)
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
-    return
+    return 1
 fi
 
-if [[ -d $1 ]]; then
+if [ -d "$1" ]; then
     echo "$1 is a directory"
-    nautilus "$1"
+    xdg-open "$1" >/dev/null 2>&1
     return
 fi
 
-if [ "$1" == '.' ]; then
-  nautilus .
-  return
-fi
-
-
-if [ "$extension" == "sh" ]; then
-  chmod +x "$1"
-  ./"$1"
-  return
-fi
-
-if [ "$extension" == "py" ]; then
- python3 "$1" 
- return
-fi
-
-if [ "$extension" == "pdf" ]; then
-    gio open "$1"
+if [ "$1" = "." ]; then
+    xdg-open . >/dev/null 2>&1
     return
 fi
 
-if [ "$extension"  == "py" ]; then
-  python3 "$1"
-  return
+if [ "$extension" = "sh" ]; then
+    chmod +x "$1"
+    "./$1"
+    return
 fi
 
-if [ "$extension" == "deb" ]; then
-  sudo dpkg -i "$1"
-  return
+if [ "$extension" = "py" ]; then
+    python3 "$1"
+    return
 fi
 
-if [ "$extension" == "jar" ]; then
-  java -jar "$1"
-  return
+if [ "$extension" = "pdf" ]; then
+    open_file "$1"
+    return
 fi
 
-for i in "${office[@]}"; 
-do 
-  if [ "$extension" == "$i" ]; then
-    libreoffice "$1"
-  fi
-done
-
-
-for i in "${text[@]}"; 
-do 
-  if [ "$extension" == "$i" ]; then
-    featherpad "$1"
-  fi
-done
-
-for i in "${images[@]}"; 
-do 
-  if [ "$extension" == "$i" ]; then
-    feh "$1"
-  fi
-done
-
-for i in "${video[@]}"; 
-do 
-  if [ "$extension" == "$i" ]; then
-    vlc "$1"
-  fi
-done
-
-grep -q "\x00" $1 && value="Binary" || value="Text"
-
-if [ "$flag" == 0 ] && [ "$value" == "Text" ]; then
-  vim "$1"
-  flag=0
-  return
+# Check if the file extension is supported and open the file using the associated application
+if [ "${file_types[$extension]+_}" ]; then
+    "${file_types[$extension]}" "$1"
+    return
 fi
 
-return
+# If the file is a text file, open it in Vim
+file -b --mime-encoding "$1" | grep -q "charset=us-ascii" && open_file "$1" || vim "$1"
+
